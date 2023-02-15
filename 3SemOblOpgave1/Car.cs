70cf36bbc067171c2cc6257219b096838daf4bbe
Simon Søby <sb.simonzone@gmail.com>
2023-02-15 19:24:44 +0100

    Add project files.

---
+++ 3SemOblOpgave1/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3SemOblOpgave1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public string LicensePlate { get; set; }
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+        public void Validate()
+        {
+            if (Model.Length < 4)
+            {
+                throw new ArgumentException("Model must be at least 4 characters long");
+            }
+            if (Price <= 0)
+            {
+                throw new ArgumentException("Price must be greater than 0");
+            }
+            if (LicensePlate.Length < 2 || LicensePlate.Length > 7)
+            {
+                throw new ArgumentException("License plate must be between 2 and 7 characters long");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Model}, {Price} kr, {LicensePlate}";
+        }
+    }
+}
---
+++ 3SemOblOpgave1Tests/CarTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3SemOblOpgave1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3SemOblOpgave1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car(1, "Audi", 100000, "AB12345");
+        Car carWithInvalidModel = new Car(1, "A", 100000, "AB12345");
+        Car carWithInvalidPrice = new Car(1, "Audi", -1, "AB12345");
+        Car carWithInvalidLicensePlate = new Car(1, "Audi", 100000, "A");
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            car.Validate();
+        }
+        
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => carWithInvalidModel.Validate());
+            Assert.ThrowsException<ArgumentException>(() => carWithInvalidPrice.Validate());
+            Assert.ThrowsException<ArgumentException>(() => carWithInvalidLicensePlate.Validate());
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.AreEqual("1. Audi, 100000 kr, AB12345", car.ToString());
+        }
+    }
+}
